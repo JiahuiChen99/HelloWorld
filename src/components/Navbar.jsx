edada49798b81dc075d4fd61652b8a0d74a7db4b
Jiahui Chen <Jiahui1@hotmail.es>
2021-05-30 21:46:39 +0200

    #1 feat: Container Navbar - Elements justified right - Navbar centered

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import Container from "react-bootstrap/Container";
 
 function NavBar() {
 
     return (
         <Navbar expand="md" fixed="top" bg="dark" variant="dark">
-            <Navbar.Brand href="#home">Jiahui</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="mr-auto">
-                    <Nav.Link href="#about">Whoami</Nav.Link>
-                    <Nav.Link href="#projects">Projects</Nav.Link>
-                    <Nav.Link href="#resume">Resume</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+            <Container>
+                <Navbar.Brand href="/">Jiahui</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#about">Whoami</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#resume">Resume</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
         </Navbar>
     )
 }
