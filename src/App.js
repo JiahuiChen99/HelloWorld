c8f7b8700f30785048ebb28779cfbf2436ca4556
Jiahui Chen <Jiahui1@hotmail.es>
2021-05-31 15:22:06 +0200

    #3 feat: Home Greetings

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
-import './App.css';
 import React from "react";
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
 
 // Components
 import NavBar from './components/Navbar';
 import Footer from "./components/Footer";
+import HomePage from "./components/Home/Home";
 
 function App() {
     return (
-        <BrowserRouter>
+        <Router>
             <NavBar/>
+                <Switch>
+                    <Route path="/" component={HomePage}/>
+                </Switch>
             <Footer/>
-        </BrowserRouter>
+        </Router>
     );
 }
 
