fc7161ea5751bf4378f96fd85fd052b8ae9c9b7f
Jiahui Chen <37770349+JiahuiChen99@users.noreply.github.com>
2021-06-01 20:35:08 +0200

    #11 refactor: Functional react converted to class

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,41 +7,43 @@ import HomeIntroduction from "./HomeIntroduction";
 import pyong from "../../Assets/pyong.gif";
 import dev from "../../Assets/home_dev.svg";
 
-function HomePage () {
-    function goToIntroduction() {
+class HomePage extends React.Component{
+    goToIntroduction() {
         console.log("Go To Introduction");
     }
 
-    return (
-        <section id="home">
-            <Container fluid className="home">
-                <Container id="home-greeting" className="d-flex flex-column justify-content-center">
-                    <Row className="home-greeting d-flex align-items-center">
-                        <Col md={7} className="home-greeting-text ">
-                            <div>
-                                <Row>
-                                    <div>Hello World!</div>
-                                    <img className="ml-4" src={pyong} alt="Pyong!" />
-                                </Row>
-                                <Row>
-                                    <div> I AM <strong className="home-name">JIAHUI</strong></div>
-                                </Row>
-                            </div>
-                        </Col>
-                        <Col md={5} className="align-content-center">
-                            <img className="img-fluid" src={dev} alt="dev"/>
-                        </Col>
-                    </Row>
-                    <Row className="d-flex justify-content-center home-go-to-introduction">
-                        <IoCaretDown size={90} onClick={ () => goToIntroduction()}/>
-                    </Row>
+    render() {
+        return (
+            <section id="home">
+                <Container fluid className="home">
+                    <Container id="home-greeting" className="d-flex flex-column justify-content-center">
+                        <Row className="home-greeting d-flex align-items-center">
+                            <Col md={7} className="home-greeting-text ">
+                                <div>
+                                    <Row>
+                                        <div>Hello World!</div>
+                                        <img className="ml-4" src={pyong} alt="Pyong!" />
+                                    </Row>
+                                    <Row>
+                                        <div> I AM <strong className="home-name">JIAHUI</strong></div>
+                                    </Row>
+                                </div>
+                            </Col>
+                            <Col md={5} className="align-content-center">
+                                <img className="img-fluid" src={dev} alt="dev"/>
+                            </Col>
+                        </Row>
+                        <Row className="d-flex justify-content-center home-go-to-introduction">
+                            <IoCaretDown size={90} onClick={this.goToIntroduction}/>
+                        </Row>
+                    </Container>
+
+                    <HomeIntroduction/>
+
                 </Container>
-
-                <HomeIntroduction/>
-
-            </Container>
-        </section>
-    );
+            </section>
+        );
+    }
 }
 
 export default HomePage;
--- src/components/Home/HomeIntroduction.jsx
+++ src/components/Home/HomeIntroduction.jsx
@@ -4,46 +4,49 @@ import { Row, Col, Container } from "react-bootstrap";
 // Image
 import bananyako from "../../Assets/bananyako.png";
 
-function HomeIntroduction () {
-    return(
-        <Container className="d-flex align-items-center" id="home-introduction">
-            <Row>
-                <Col md={4} className="d-flex align-items-center">
-                    <img className="img-fluid" src={bananyako} alt="bananyako"/>
-                </Col>
-                <Col md={8}>
-                    <h1> LET ME <span className="home-text-highlight">INTRODUCE</span> MYSELF</h1>
-                    <p className="home-text-introduction mt-5">
-                        I first started coding in my first year of <span className="home-text-highlight">Computer Engineering</span> major
-                        <br/>
-                        <br/>
-                    </p>
+class HomeIntroduction extends React.Component{
 
-                    <div className="d-flex justify-content-center">
-                        <div className="home-text-introduction d-flex flex-column">
-                            <span>do</span>
-                            <span className="ml-4">Programming</span>
-                            <span>&#125; while (Alive)</span>
+    render() {
+        return(
+            <Container className="d-flex align-items-center" id="home-introduction">
+                <Row>
+                    <Col md={4} className="d-flex align-items-center">
+                        <img className="img-fluid" src={bananyako} alt="bananyako"/>
+                    </Col>
+                    <Col md={8}>
+                        <h1> LET ME <span className="home-text-highlight">INTRODUCE</span> MYSELF</h1>
+                        <p className="home-text-introduction mt-5">
+                            I first started coding in my first year of <span className="home-text-highlight">Computer Engineering</span> major
+                            <br/>
+                            <br/>
+                        </p>
+
+                        <div className="d-flex justify-content-center">
+                            <div className="home-text-introduction d-flex flex-column">
+                                <span>do</span>
+                                <span className="ml-4">Programming</span>
+                                <span>&#125; while (Alive)</span>
+                            </div>
                         </div>
-                    </div>
 
-                    <p className="home-text-introduction">
-                        <br/>
-                        <br/>
-                        Right now I am interested in <span className="home-text-highlight">Front-end</span> development with Modern JavaScript Frameworks,
-                        but eventually I would like become a <span className="home-text-highlight">Full-stack</span> Software Engineer
-                        <br/>
-                        <br/>
-                       Thinking out the box is in my coded in my &lt;DNA/&gt;
-                    </p>
+                        <p className="home-text-introduction">
+                            <br/>
+                            <br/>
+                            Right now I am interested in <span className="home-text-highlight">Front-end</span> development with Modern JavaScript Frameworks,
+                            but eventually I would like become a <span className="home-text-highlight">Full-stack</span> Software Engineer
+                            <br/>
+                            <br/>
+                            Thinking out the box is in my coded in my &lt;DNA/&gt;
+                        </p>
 
-                    <div className="d-flex justify-content-center mt-5">
-                        <a type="button" className="btn btn-lg btn-outline-primary" href="/whoami"> Learn more about me </a>
-                    </div>
-                </Col>
-            </Row>
-        </Container>
-    )
+                        <div className="d-flex justify-content-center mt-5">
+                            <a type="button" className="btn btn-lg btn-outline-primary" href="/whoami"> Learn more about me </a>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default HomeIntroduction;
