51e11068964254c4df94262a864ff70dcd993469
Jiahui Chen <37770349+JiahuiChen99@users.noreply.github.com>
2021-06-02 17:36:58 +0200

    #8 feat: Project Page headers

--- src/components/Project/Project.jsx
+++ src/components/Project/Project.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
+import Particle from "../Particle";
 
 function Project() {
-    return(
+    {/* Fetch Github API for Projects*/}
 
+    return(
+        <section id="project">
+            <Particle/>
+            <Container className="d-flex justify-content-center">
+                <Col md={12}>
+                    <Row className="d-flex align-items-center">
+                        <Col md={12} className="full-height d-flex align-items-center">
+                            <h1 className="project-featured-title">Featured Projects</h1>
+                            {/* Featured Projects Component */}
+                        </Col>
+                    </Row>
+                    <Row md={12} className="full-height">
+                        <h1 className="project-featured-title"><strong>Other Noteworthy Projects</strong></h1>
+                    </Row>
+                </Col>
+            </Container>
+        </section>
     );
 }
 
