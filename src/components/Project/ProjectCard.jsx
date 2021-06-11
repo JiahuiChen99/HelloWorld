92968194d2ad7a9e211bd3ee8382866c541910d6
Jiahui Chen <37770349+JiahuiChen99@users.noreply.github.com>
2021-06-11 18:56:19 +0200

    #9 feat: Fetch project languages

--- src/components/Project/ProjectCard.jsx
+++ src/components/Project/ProjectCard.jsx
@@ -1,17 +1,40 @@
 import React from "react";
 import './ProjectCard.css'
+import axios from "axios";
 
 class ProjectCard extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            languages : {}
+        }
+    }
+
+    componentDidMount() {
+        axios.get(this.props.project.languages_url)
+            .then( response => {
+                this.setState({
+                    languages : response.data
+                })
+        });
     }
 
     render() {
+        const languages = Object.keys(this.state.languages);
+
         return(
-            <div className="card bg-dark" key={ this.props.id } >
+            <div className="card bg-dark h-100" key={ this.props.id } >
                 <div className="card-body">
                     <h5 className="card-title text-primary">{ this.props.project.name }</h5>
                     <p className="card-text text-warning">{ this.props.project.description}</p>
+                </div>
+                <div className="card-footer">
+                    {
+                        languages.forEach( (lang, index) => (
+                            <div className="card-text text-danger" key={index} onLoad={console.log(lang)}> { lang } </div>
+                        ))
+                    }
                 </div>
             </div>
         );
