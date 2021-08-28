eb3ebe4b0fb09fbc4ca44347212943afb5f5362e
JiahuiChen99 <Jiahui1@hotmail.es>
2021-08-28 12:55:12 +0200

    #23 feat: Experience section - Apprenticeship and Internship experience as constant data

---
+++ src/components/Whoami/Experience.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Container from "react-bootstrap/Container";
+
+function Experience() {
+    const experience = [
+        {
+            title: "Teacher Assistant in Systems Administration & Design @ La Salle URL",
+            type: "Internship",
+            brief_desc: "In charge of designing & developing the projects, labs correction and helping students solve their duties",
+            tags: ["C", "LKM", "Linux", "Raspberry Pi"],
+            description: "The 1st project consists in building a Linux Kernel Module (LKM) to be deployed in a Raspberry Pi Zero W. The device works as a character device driver where the user can perform reads and writes"
+        },
+        {
+            title: "Build@Mercari (株式会社メルカリ)",
+            type: "Apprenticeship",
+            brief_desc: "Improve software engineering skills through the training",
+            tags: [],
+            description: "Improve software engineering skills through the training\n" +
+                "\n" +
+                "Values from Build@Mercari:\n" +
+                "- Study lots of Udemy courses\n" +
+                "- Met new friends from around the world\n" +
+                "- Mercari Engineering culture & DI philosophy"
+        },
+    ];
+
+    return (
+        <Container fluid>
+
+        </Container>
+    );
+}
+
+export default Experience;
