d0fa79c6e8c02bfabed3d05c1ca56c3fdceaa95b
JiahuiChen99 <Jiahui1@hotmail.es>
2021-08-28 19:43:45 +0200

    #26 feat: Basic layout for

---
+++ src/components/Whoami/ExperiencePopUp.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+class ExperiencePopUp extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <div className="bg-dark h-100" key={ this.props.experience.id }>
+                <div className="card-body">
+                    <div className="d-flex justify-content-between">
+                        <h1 className="card-title font-weight-bold"> { this.props.experience.title}</h1>
+                        <span className="badge bg-secondary align-self-center text-uppercase">
+                            <h4>
+                                { this.props.experience.type }
+                            </h4>
+                        </span>
+                    </div>
+
+                    <p className="fs-1"> { this.props.experience.brief_desc } </p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ExperiencePopUp;
