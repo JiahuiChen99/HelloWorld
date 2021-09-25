27609c4ad7505ada616887f5da9e23e585c4ab4b
JiahuiChen99 <Jiahui1@hotmail.es>
2021-09-25 22:02:08 +0200

    #24 feat: Not programming language card - Centered logo and name - Change text color on card hover

--- src/components/Whoami/WorkflowCard.jsx
+++ src/components/Whoami/WorkflowCard.jsx
@@ -20,16 +20,22 @@ class WorkflowCard extends React.Component {
 
     render() {
         return (
-            <div className="d-flex flex-column tech-card p-3 m-1">
-                <img className="tech-card-img mb-3" src={ this.props.tech.logo }  alt={ this.props.tech.name } width="100" height="100"/>
-                <div className="tech-card-name font-weight-bold mb-3"> { this.props.tech.name } </div>
-                <p className="tech-card-text h-100"> { this.props.tech.description }</p>
-                <div className="d-flex exp row m-0 align-self-end">
-                    <div className="col exp-1" style={{ backgroundColor: this.exp(1) }}/>
-                    <div className="col exp-2" style={{ backgroundColor: this.exp(2) }}/>
-                    <div className="col exp-3" style={{ backgroundColor: this.exp(3) }}/>
+            this.props.tech.language ?
+                <div className="d-flex flex-column tech-card p-3 m-1">
+                    <img className="tech-card-img mb-3" src={ this.props.tech.logo }  alt={ this.props.tech.name } width="100" height="100"/>
+                    <div className="tech-card-name font-weight-bold mb-3"> { this.props.tech.name } </div>
+                    <p className="tech-card-text h-100"> { this.props.tech.description }</p>
+                    <div className="d-flex exp row m-0 align-self-end">
+                        <div className="col exp-1" style={{ backgroundColor: this.exp(1) }}/>
+                        <div className="col exp-2" style={{ backgroundColor: this.exp(2) }}/>
+                        <div className="col exp-3" style={{ backgroundColor: this.exp(3) }}/>
+                    </div>
                 </div>
-            </div>
+                :
+                <div className="d-flex flex-column tech-card p-3 m-1 justify-content-center align-items-center">
+                    <img src={ this.props.tech.logo }  alt={ this.props.tech.name } width="100" height="100"/>
+                    <div className="mt-2 font-weight-bold tech-name"> { this.props.tech.name } </div>
+                </div>
         )
     }
 }
