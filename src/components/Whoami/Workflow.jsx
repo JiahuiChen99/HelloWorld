f416a312527bf84fc00e9e6e26c076ae3c1525c9
JiahuiChen99 <Jiahui1@hotmail.es>
2021-09-25 21:08:03 +0200

    #24 feat: Programming languages area

--- src/components/Whoami/Workflow.jsx
+++ src/components/Whoami/Workflow.jsx
@@ -1,13 +1,29 @@
 import React from "react";
-import {Container} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
+import WorkflowCard from "./WorkflowCard";
+import "./WorkflowCard.css";
+import {techStack} from "../../Assets/data";
 
 
 class Workflow extends React.Component {
 
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
-            <Container>
-                <h1 className="ml-3 mb-3 font-weight-bold">Workflow</h1>
+            <Container className="mt-5">
+                <h1 className="ml-3 mb-3 font-weight-bold text-center">Programming Languages</h1>
+                <Row className="row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-1 justify-content-center">
+                    {
+                        techStack.languages.map( (tech, index) => (
+                            <Col className="d-flex justify-content-center my-3">
+                                <WorkflowCard key={ index } tech={ tech }/>
+                            </Col>
+                        ))
+                    }
+                </Row>
             </Container>
         )
     }
