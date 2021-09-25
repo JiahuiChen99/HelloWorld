f7148b89d7f00566f9413cc11ab7314f60fdfba5
JiahuiChen99 <Jiahui1@hotmail.es>
2021-09-25 22:07:10 +0200

    #24 feat: Environment and Tools cards

--- src/components/Whoami/Workflow.jsx
+++ src/components/Whoami/Workflow.jsx
@@ -16,9 +16,31 @@ class Workflow extends React.Component {
             <Container className="mt-5">
                 <h1 className="ml-3 mb-3 font-weight-bold">Workflow</h1>
                 <h2 className="ml-3 mb-3 font-weight-bold text-center">Programming Languages</h2>
-                <Row className="row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-1 justify-content-center">
+                <Row className="row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-1 justify-content-center mb-5">
                     {
                         techStack.languages.map( (tech, index) => (
+                            <Col className="d-flex justify-content-center my-3">
+                                <WorkflowCard key={ index } tech={ tech }/>
+                            </Col>
+                        ))
+                    }
+                </Row>
+
+                <h2 className="ml-3 mb-3 font-weight-bold text-center">Environment</h2>
+                <Row className="row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-1 justify-content-center mb-5">
+                    {
+                        techStack.environment.map( (tech, index) => (
+                            <Col className="d-flex justify-content-center my-3">
+                                <WorkflowCard key={ index } tech={ tech }/>
+                            </Col>
+                        ))
+                    }
+                </Row>
+
+                <h2 className="ml-3 mb-3 font-weight-bold text-center">Tools</h2>
+                <Row className="row-cols-lg-4 row-cols-md-3 row-cols-sm-2 row-cols-1 justify-content-center pb-5">
+                    {
+                        techStack.tools.map( (tech, index) => (
                             <Col className="d-flex justify-content-center my-3">
                                 <WorkflowCard key={ index } tech={ tech }/>
                             </Col>
