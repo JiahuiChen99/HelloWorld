e1e3edcd939805c4cc6c8b8daad8c0b33b966ea0
JiahuiChen99 <Jiahui1@hotmail.es>
2021-09-27 19:25:56 +0200

    #31 feat: Scroll to section on callback

--- src/components/Whoami/Whoami.jsx
+++ src/components/Whoami/Whoami.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import Particle from "../Particle";
 import "./Whoami.css"
 import Experience from "./Experience";
@@ -6,14 +6,37 @@ import Workflow from "./Workflow";
 import ScrollSpy from "./ScrollSpy";
 
 function Whoami() {
+    const moreaboutme = useRef(null)
+    const experience = useRef(null)
+    const workflow = useRef(null)
 
+    const goToSection = (which) => {
+        switch (which) {
+            case 0:
+                moreaboutme.current.scrollIntoView({block: 'start', inline: 'center', behavior: 'smooth'})
+                break;
+            case 1:
+                experience.current.scrollIntoView({block: 'start', inline: 'center', behavior: 'smooth'})
+                break;
+            case 2:
+                workflow.current.scrollIntoView({block: 'start', inline: 'center', behavior: 'smooth'})
+                break;
+            default:
+        }
+    }
 
     return (
         <section id="whoami">
             <Particle/>
-            <ScrollSpy/>
-            <Experience/>
-            <Workflow/>
+            <ScrollSpy goToSection={goToSection}/>
+            <section id="moreaboutme" ref={moreaboutme}>
+            </section>
+            <section id="experience" ref={experience}>
+                <Experience/>
+            </section>
+            <section id="workflow" ref={workflow}>
+                <Workflow/>
+            </section>
         </section>
     );
 }
