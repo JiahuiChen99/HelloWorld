6faa70fc3a0c8366a2389988aeed6c35122b9b71
JiahuiChen99 <Jiahui1@hotmail.es>
2021-09-30 22:42:31 +0200

    #22 feat: Interest card for collapsible

--- src/components/Whoami/InterestCard.jsx
+++ src/components/Whoami/InterestCard.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./InterestCard.css"
 
 class InterestCard extends React.Component {
 
@@ -8,10 +9,10 @@ class InterestCard extends React.Component {
 
     render() {
         return(
-            <div>
-                <img src={ this.props.interest.image } alt={ this.props.interest.name }/>
-                <div> { this.props.interest.original_name } </div>
-                <div> { this.props.interest.name } </div>
+            <div className="d-flex flex-column">
+                <img className="interest-image" src={ this.props.interest.image } alt={ this.props.interest.name } height="350"/>
+                <div className="text-center font-weight-bold"> { this.props.interest.original_name } </div>
+                <div className="text-center font-weight-light"> { this.props.interest.name } </div>
             </div>
         )
     }
